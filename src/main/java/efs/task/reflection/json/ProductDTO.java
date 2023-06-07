5d49b15dc7b00a0e187ddeb5a72d82bfac489fa5
Kondro571 <133992948+Kondro571@users.noreply.github.com>
2023-06-07 19:45:47 +0200

    Update ProductDTO.java

--- src/main/java/efs/task/reflection/json/ProductDTO.java
+++ src/main/java/efs/task/reflection/json/ProductDTO.java
@@ -1,3 +1,5 @@
+package efs.task.reflection.json;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
